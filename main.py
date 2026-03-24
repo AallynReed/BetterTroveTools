1e905392bae47acbf29f58dcfba3f59a95264b2f
Aallyn <support@aallyn.xyz>
2026-03-24 22:27:06 +0000

    fix: Change working directory to base_dir when running in frozen mode

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if getattr(sys, 'frozen', False):
     base_dir = os.path.dirname(sys.executable)
     if not hasattr(sys, '_MEIPASS'):
         sys._MEIPASS = base_dir
+    os.chdir(base_dir)
 else:
     base_dir = os.path.dirname(os.path.abspath(__file__))
 
